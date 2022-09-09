e98ee4133256dcac2a1a91e882d5cecad9fdfc38
Tewodros Hailye <helunat9@gmail.com>
2022-09-09 11:23:29 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - to print whether a number is positive negative or zer0
+ * Return: Always 0 (Success)
 int main(void)
 {
-/*declaration of variable*/
 int n;
      printf("enter the number: ");
      scanf("%d", n);
-     if (n>0)
+     if (n > 0)
      {
-	printf("The number is positive\n");
+	printf("%d is positive\n");
 }
-else if (n==0)
+else if (n == 0)
 {
-	 printf("The number is zero\n");
+	 printf("%d is zero\n");
 }
 else 
- printf("The number is negative");
- return 0;
+ printf("%d is negative");
+ return (0);
  }
