5bc28f048f0572730d267257c3e135d3b143ae0e
Tewodros Hailye <helunat9@gmail.com>
2022-09-08 17:16:23 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 (success)
+ * Return : return the text
  */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
