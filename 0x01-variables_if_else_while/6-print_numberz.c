eadb30555ce0e415990a80c7c2597651c6a7e915
Tewodros Hailye <helunat9@gmail.com>
2022-09-09 16:39:05 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - printing numbers 
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((n%10) + '0');
+	}
+	putchar('\n');
+	return (0);
+}
