37de583aa0dda05e40b6db3efdcd3f287a2c0ff6
Tewodros Hailye <helunat9@gmail.com>
2022-09-14 04:14:58 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to print the word _putchar
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char *a;
+
+	while (*a)
+	{
+		_putchar(*a);
+		_putchar('\n');
+	}
+	return (0);
+}
