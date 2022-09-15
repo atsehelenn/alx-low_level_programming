d6c3223c9d28d6f247cb2943d5b86a532a126040
Tewodros Hailye <helunat9@gmail.com>
2022-09-15 15:51:23 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * print_numbers - print numbers 0-9
+ * Return: void
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
