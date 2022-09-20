98387d98eee884b626e673f31f105bc04a0097dc
Tewodros Hailye <helunat9@gmail.com>
2022-09-20 12:12:36 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - call for function
+ * Return: Always 0
+*/
+int main(void)
+{
+	int a, b;
+
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
