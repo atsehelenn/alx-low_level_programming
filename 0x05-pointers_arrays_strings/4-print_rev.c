ce5bee85686b773792d08fdaa7f263b6b08a9a98
Tewodros Hailye <helunat9@gmail.com>
2022-09-20 13:50:10 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rev - reverse a string
+ * @s: string to be reversed
+ * Return: void
+*/
+void print_rev(char *s)
+{
+	int a, b, c;
+	char *d, ted;
+        
+	d = s;
+	while (s[c] != '\n')
+	{
+		c++;
+	}
+	for (b = 1; b < c; b++)
+	{
+		d++;
+	}
+	for (a = 0; a < (c/2); a++)
+	{
+		ted = s[a];
+		s[a] = *d;
+		*d = ted;
+		d--;
+	}
+}
+
